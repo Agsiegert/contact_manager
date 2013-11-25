d4c04f90b77dc1e57868262641026476e7a81b73
Antony Siegert <agsiegert@hotmail.com>
2013-11-25 15:36:12 -0700

    Generate inital contact mgr project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactManager::Application.config.secret_key_base = 'd5d8230ce2bedb7bde67f5ab468e798c60e10e03033fec4a9d2e32a46cc56497b1e7c0306d2453035faefc5bb3c5e7f4e1798b6471febea893ead40a11e466c6'
